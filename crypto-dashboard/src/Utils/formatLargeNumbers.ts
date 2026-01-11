4c43ac2890e168effa21db66d6568745ab4879fd
Molly DJ <mollydj@mac.home>
2026-01-11 22:18:40 +0100

    Updates

--- crypto-dashboard/src/Utils/formatLargeNumbers.ts
+++
@@ -1,6 +0,0 @@
-export const formatLargeNumber = (num: number) => {
-  if (Math.abs(num) >= 1_000_000_000) return (num / 1_000_000_000).toFixed(2) + 'B';
-  if (Math.abs(num) >= 1_000_000)     return (num / 1_000_000).toFixed(2) + 'M';
-  if (Math.abs(num) >= 1_000)         return (num / 1_000).toFixed(2) + 'K';
-  return num.toFixed(2);
-};
