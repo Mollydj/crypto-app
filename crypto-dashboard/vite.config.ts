f1a635ca6d1678f95270204153c19731e4db5fbd
Molly DJ <mollydj@mac.home>
2026-01-11 22:08:21 +0100

    Github Deploy

--- crypto-dashboard/vite.config.ts
+++ crypto-dashboard/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/crypto-app/", // <-- your GitHub repo name
+  base: "/crypto-app/",
   optimizeDeps: {
     include: ["react", "react-dom"],
   },
