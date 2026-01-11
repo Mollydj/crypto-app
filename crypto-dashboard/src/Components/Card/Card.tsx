2c353f89503af419cf1814fac01803a25078c78f
Molly DJ <mollydj@mac.home>
2026-01-11 22:20:22 +0100

    Fixed broken scripts

---
+++ crypto-dashboard/src/Components/Card/Card.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Card.less";
+import { useCurrency } from "../../Utils/CurrencyContext";
+import { Avatar, Card, Flex, Skeleton } from "antd";
+import CryptoButton from "../Button/Button";
+import type { Coin } from "../../Hooks/useCryptoList";
+
+interface CardProps {
+  coins: Coin[];
+  currency: string;
+  loading: boolean;
+  livePrices: Record<string, string>;
+}
+
+const CryptoCard: React.FC<CardProps> = ({
+  coins = [],
+  livePrices,
+  loading,
+}) => {
+  const { currency } = useCurrency();
+
+  return (
+    <div className="card-container">
+      {coins.map((coin: any) => {
+        const pair = `${coin.symbol.toUpperCase()}-${currency}`;
+        const priceNumber = Number(livePrices[pair] ?? coin.current_price);
+        return (
+          <Skeleton
+            active={!coins || loading}
+            loading={!coins || loading}
+            key={coin.id}
+          >
+            <Flex vertical>
+              <Card key={coin.id} className="crypto-card">
+                <Avatar shape="square" src={coin.image}>
+                  {coin.symbol?.toUpperCase()?.[0]}
+                </Avatar>
+                <span>
+                  {coin.market_cap_rank}. {coin.name} (
+                  {coin.symbol.toUpperCase()})
+                </span>
+                <CryptoButton className="card-button">
+                  {priceNumber.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: currency,
+                  })}
+                </CryptoButton>
+                <span
+                  className={
+                    coin.market_cap_change_percentage_24h < 0
+                      ? "negative"
+                      : "positive"
+                  }
+                >
+                  {coin.market_cap_change_percentage_24h > 0 ? "+" : ""}
+                  {coin.market_cap_change_percentage_24h.toFixed(2)}%
+                  <br />
+                </span>
+              </Card>
+            </Flex>
+          </Skeleton>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CryptoCard;
