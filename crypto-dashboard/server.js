8fa698fb53bc10f224f2ea04006e59f6e151f628
Molly DJ <mollydj@mac.home>
2026-01-14 19:01:02 +0100

    Prep for deployment

--- crypto-dashboard/server.js
+++ crypto-dashboard/server.js
@@ -66,5 +66,5 @@ app.get("/api/cryptoImage", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 
-app.listen(PORT, "0.0.0.0", () => console.log("Server running on port 3001"));
